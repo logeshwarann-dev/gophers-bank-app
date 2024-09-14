755e6249484d4c314e35610d44ec47faac3a1eb5
Logeshwaran-N <logeshwaran082@gmail.com>
2024-09-14 20:01:20 +0530

    Create/Get Account APIs were implemented

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/logeshwarann-dev/gophers-bank-app/api"
+	db "github.com/logeshwarann-dev/gophers-bank-app/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/gopher_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("Cannot start the server:", err)
+	}
+
+}
