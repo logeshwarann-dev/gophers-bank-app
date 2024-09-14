e34e7bdadeb1f01cc04c860b835918cf6fa872a3
Logeshwaran-N <logeshwaran082@gmail.com>
2024-09-14 11:20:49 +0530

    Updated workflow file with postgres port

--- db/install_migrate.sh
+++ db/install_migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://packagecloud.io/golang-migrate/migrate/gpgkey | apt-key add -
-echo "deb https://packagecloud.io/golang-migrate/migrate/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/migrate.list
-apt-get update
-apt-get install -y migrate
+sudo curl -L https://packagecloud.io/golang-migrate/migrate/gpgkey | apt-key add -
+sudo echo "deb https://packagecloud.io/golang-migrate/migrate/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/migrate.list
+sudo apt-get update
+sudo apt-get install -y migrate
